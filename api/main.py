f904d0db320b8940d48a61df2ac72950f2ec6705
Anton A Shanaurin <grspectre@gmail.com>
2024-03-29 12:04:31 +0300

    server config

--- api/main.py
+++ api/main.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"success": True, "message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"success": True, "message": "Hello World"}
 
 
-@app.get("/ping")
+@app.get("/api/ping")
 async def ping():
     return {"success": True}
 
